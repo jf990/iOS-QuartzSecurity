c2336a7e35ec9c5a0968d7b276078ca9b942bd14
John Foster <jfoster@esri.com>
2016-04-18 16:03:07 -0700

    Initial Commit

---
+++ runtime-security-auth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  runtime-security-auth
+//
+//  Created by John Foster on 4/18/16.
+//  Copyright © 2016 John Foster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
